19b3207f30ba60929595f549972ac8e063a39353
kenny-tech <kenny4real2001@gmail.com>
2019-08-06 11:47:08 +0100

    Get restaurants

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -8,12 +8,24 @@ export default class RestaurantList extends Component {
         return (
             <React.Fragment>
                 <RestaurantConsumer>
-                    {value => {
-                        // return (<div>
-                                
-                        //     </div>
-                        // )
-                        console.log(value);
+                    {(value) => {
+                        //console.log(value);
+                        const res  = value.restaurants;
+                        //console.log(typeof res)
+                        let obj, restaurant;
+                        for(obj in res) {
+                            restaurant = res[obj].map(row => (<li className="list-group-item" key={row.name}>{row.name}</li>));
+                            return(
+                            <div className="row">
+                                <div className="col-12">
+                                    <h3 className="text-center">Restaurants</h3>
+                                    <ul className="list-group">
+                                        {restaurant}
+                                    </ul>
+                                </div>
+                            </div>
+                            )
+                        }
                     }}
                 </RestaurantConsumer>
             </React.Fragment>
